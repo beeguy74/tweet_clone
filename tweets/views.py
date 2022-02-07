3902cc4d3a49775e83b0256ba6bdeda7f8518cd5
beeguy <beeguy74@gmail.com>
2022-02-07 16:35:48 +0300

    some errors handling

--- tweets/views.py
+++ tweets/views.py
@@ -25,6 +25,9 @@ def tweet_create_view(request, *args, **kwargs):
 		if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
 			return redirect(next_url)
 		form = TweetForm()
+	if form.errors:
+		if request.is_ajax():
+			return JsonResponse(form.errors, status=400) #response with error generated in TweetForm class
 	return render(request, 'components/forms.html', context={"form": form})
 
 
