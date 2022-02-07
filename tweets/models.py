509a75eb69c04b931f5975a183b5eedf0afa0bf0
beeguy <beeguy74@gmail.com>
2022-02-07 16:15:22 +0300

    new tweet rendering on top now

--- tweets/models.py
+++ tweets/models.py
@@ -8,6 +8,11 @@ class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
+    # changing meta option gives us a descending order tweets on page
+    # but dont forget makemigrations
+    class Meta:
+        ordering = ['-id']
+
     def serialize(self):
         return {
             "id": self.id,
