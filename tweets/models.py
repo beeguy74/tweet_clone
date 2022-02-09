f30d6d4af764eddd90708225e4587c50b5fef770
beeguy <beeguy74@gmail.com>
2022-02-09 13:48:32 +0300

    add some errors handling in back and front

--- tweets/models.py
+++ tweets/models.py
@@ -1,10 +1,17 @@
 from distutils.command.upload import upload
 import random
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL #builtin django user model is great!
 
 
 class Tweet(models.Model):
+    # maps to sql
     # id = models.AutoField(primary_key=True)
+    # many users can have many tweets
+    # user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) #keeps tweets
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # delete all twets if user was deleted
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
