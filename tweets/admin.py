e06fa2301da5c0b2afcd22123bd257f42db90573
beeguy <beeguy74@gmail.com>
2022-02-09 15:07:12 +0300

    /admin tune

--- tweets/admin.py
+++ tweets/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Tweet
 
-admin.site.register(Tweet)
+
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'user'] #__str__ method is defaulting to Tweet model
+    search_fields = ['content', 'user__username']
+    class Meta:
+        model = Tweet
+
+
+admin.site.register(Tweet, TweetAdmin) #adding searchfield to /admin/tweets search by user
